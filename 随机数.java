131eec71c25faa2dd5c8ce1cd63a8edb4db24457
Zhang-Jia-ju <1375246845@qq.com>
2022-04-03 17:53:54 +0800

    Update 随机数.java

--- 随机数.java
+++ 随机数.java
@@ -1,14 +1,18 @@
-package com.company.java;
-import java.util.Random;//导入包
-public class helloworld{
-    public static void main(String[] args){
-        //创建对象
-        Random r =new Random();
-        int number =r.nextInt(10);//这里数据的范围是0到9，不包括10
-        //int number =r.nextInt(10)+1;//这样写可以确保数据范围是1到10，其他类似
-        System.out.println("number ="+number);
-
-
+import java.util.*;//星号可以一次导入改包中的所有类
+//写一则程序目的是输出n个随机数，并且每次输出一个都要有回车
+    public class App {
+        public static void main(String[] args){
+          System.out.print("请输入n的值，来确定最终生成随机数的个数:");
+          Scanner in = new Scanner(System.in);
+          Random r= new Random(); //声明类的对象(找一条具体的狗）
+          int number = in.nextInt();//这里是输入语句,对象in使用其方法nextInt
+          in.close();//记得关闭Scanner
+          int i=1,a;
+          while(i<=number)//很简单的while循环
+          {
+            a = r.nextInt(100)+1;//这里是输入1到100之间的随机数,即0+1~max+1
+            i++;
+            System.out.println(a);
+        }
+      }
     }
-
-}
